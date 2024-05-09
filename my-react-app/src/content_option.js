330af61c1788c4d797e15e0b66864fd0c8ae6d80
Jack Lowe <jack@jacklowe.dev>
2024-05-09 11:21:44 +0000

    template + editing

---
+++ my-react-app/src/content_option.js
@@ -0,0 +1,99 @@
+const logotext = "Jack Lowe";
+const meta = {
+    title: "Jack Lowe",
+    description: "I’m Jack Lowe",
+};
+
+const introdata = {
+    title: "I’m Jack Lowe",
+    animated: {
+        first: "I love coding",
+        second: "I code cool projects",
+        third: "cybersecurity enthusiast",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A little bit about myself",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+
+//const worktimeline = [{
+//        jobtitle: "Designer of week",
+//        where: "YAdfi",
+//        date: "2020",
+//    },
+//    {
+//        jobtitle: "Designer of week",
+//        where: "Jamalya",
+//        date: "2019",
+//    },
+//    {
+//        jobtitle: "Designer of week",
+//        where: "ALquds",
+//        date: "2019",
+//    },
+//];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "HTML",
+        value: 80,
+    },
+    {
+        name: "CSS",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "/assets/StampWorks-Cover.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jack@jacklowe.dev",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Jack-Lowe-7/",
+    linkedin: "https://linkedin.com/in/jacklowe7",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
