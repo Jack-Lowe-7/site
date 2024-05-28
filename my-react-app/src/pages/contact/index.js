026ef77ad595c820239543cd3c30e365a67d3e3b
Jack Lowe <jack@jacklowe.dev>
2024-05-28 14:56:37 +0000

    fixed contact page + added project page for stampworks (blank)

--- my-react-app/src/pages/contact/index.js
+++ my-react-app/src/pages/contact/index.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
-import * as emailjs from "emailjs-com";
+import React from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { meta } from "../../content_option";
-import { Container, Row, Col, Alert } from "react-bootstrap";
-import { contactConfig } from "../../content_option";
+import { Container, Row, Col } from "react-bootstrap";
+import { contactConfig, socialprofils } from "../../content_option";
+import {
+  FaEnvelope,
+  FaLinkedin,
+} from "react-icons/fa";
 
 export const ContactUs = () => {
   return (
@@ -15,47 +18,16 @@ export const ContactUs = () => {
           <title>{meta.title} | Contact</title>
           <meta name="description" content={meta.description} />
         </Helmet>
-        <Row className="mb-5 mt-3 pt-md-3">
-          <Col lg="8">
-            <h1 className="display-4 mb-4">Contact Me</h1>
-            <hr className="t_border my-4 ml-0 text-left" />
-          </Col>
-        </Row>
+        <h1 className="display-4 mb-4">Contact Me</h1>
+        <hr className="t_border my-4 ml-0 text-left" />
+        <h3 className="color_sec py-4">Get in touch</h3>
+        <p>{contactConfig.description}</p>
         <Row className="sec_sp">
-          <Col lg="12">
-            <Alert
-              //show={formData.show}
-              variant={formData.variant}
-              className={`rounded-0 co_alert ${
-                formData.show ? "d-block" : "d-none"
-              }`}
-              onClose={() => setFormdata({ show: false })}
-              dismissible
-            >
-              <p className="my-0">{formData.alertmessage}</p>
-            </Alert>
-          </Col>
           <Col lg="5" className="mb-5">
-            <h3 className="color_sec py-4">Get in touch</h3>
-            <address>
-              <strong>Email:</strong>{" "}
-              <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
-                {contactConfig.YOUR_EMAIL}
-              </a>
-              <br />
-              <br />
-              {contactConfig.hasOwnProperty("YOUR_FONE") ? (
-                <p>
-                  <strong>Phone:</strong> {contactConfig.YOUR_FONE}
-                </p>
-              ) : (
-                ""
-              )}
-            </address>
-            <p>{contactConfig.description}</p>
+            <a href={socialprofils.email}><FaEnvelope /></a>  Email me at <a href={socialprofils.email}>jack@jacklowe.dev</a><br></br>
+            <a href={socialprofils.linkedin}><FaLinkedin /></a>  Message me on <a href={socialprofils.linkedin}>LinkedIn</a><br></br>
           </Col>
         </Row>
       </Container>
-      <div className={formData.loading ? "loading-bar" : "d-none"}></div>
     </HelmetProvider>
 )}
